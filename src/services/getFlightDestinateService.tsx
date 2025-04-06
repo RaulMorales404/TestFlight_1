092b67dfa6e0f267e1ef83787140404aafe581e7
RaulMorales404 <r20160106@gmail.com>
2025-04-06 04:34:01 -0600

    Implementados los servicios necesarios para buscar vuelos.

--- src/services/getFlightDestinateService.tsx
+++ src/services/getFlightDestinateService.tsx
@@ -1,17 +1,25 @@
-// src/services/postService.ts
-import { Endpint } from '@api/endPoints';
 import axios from 'axios';
+import {Endpint} from '@api/endPoints';
+import {FlightStatusCollection} from '@models/destinationFlight';
 
 export const getFlightDestinateService = async (
-  origin: string,
-  Destination: string,
-) => {
-
-    try {
-        const response = await axios.get(Endpint.DESTINATE_API_URL_GET);
-        return response.data;
-      } catch (error) {
-        throw new Error('Error al obtener el post');
-      }
-
+  numberFlight: string,
+): Promise<FlightStatusCollection[]> => {
+  try {
+   
+    const response = await axios.get(Endpint.DESTINATE_API_URL_GET);
+    const flightData = require('./../assets/data/OrigenDestinoResponse.json');
+    if (numberFlight) {
+      const filterData = flightData.flightStatusCollection.filter(
+        ({segment}: FlightStatusCollection) =>
+          segment.operatingFlightCode === numberFlight,
+      );
+      return filterData;
+    } else {
+      return flightData.flightStatusCollection;
+    }
+  } catch (error) {
+    console.error('Error al obtener los datos:', error);
+    throw new Error('Error al obtener los datos');
+  }
 };
